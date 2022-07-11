b6e7435d7a9b5a3b14e8ebd901d9d96e3824f23f
NineTailSecurity <105800796+NineTailSecurity@users.noreply.github.com>
2022-07-11 00:55:49 -0400

    Add files via upload

---
+++ MemeBot.py
@@ -0,0 +1,32 @@
+import discord
+import os
+import requests
+import json
+
+TOKEN = "OTk1OTEwMjE1Mzk0NDEwNTU2.G_FDP2.jIUpGA4tb45mUwjp0uf8Gy8wXB4viHx0YcMBt4"
+client = discord.Client()
+
+
+def get_meme():
+    response = requests.get("https://generatorfun.com/")
+    json_data = json.loads(response.text)
+    meme = json_data[0]['q'] + " -" + json_data[0]['a']
+    return meme
+
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('I WANT A MEME'):
+        meme = get_meme()
+        await message.channel.send(meme)
+
+client.run(os.getenv(TOKEN))
