efa623aae7ce575825221e968dd551aad191fa99
Daniel Maya <danidd0213@gmail.com>
2022-07-11 19:55:57 -0400

    Updated Print Information for User

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('User is logged with credentials {0.user}'.format(client))
 
 
 @client.event
